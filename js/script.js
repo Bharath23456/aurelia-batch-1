5983c640df559105c81ec2a6ca3834868b94f675
K R BHARATH <bharath899090@gmail.com>
2025-05-24 14:22:16 +0530

    intial message

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-function submitFunction(){
-  console.log("hi");
+let students={
+  name1:"bharath",
+  age:20,
+  address:{
+    pincode:583125
+  }
 }
+
+// console.log(students.name,students.age,students.pincode);
+
+let {name1,age}=students;
+console.log(name1,age)
